f3501061a79d08ad4fb79490a5025cc6aff85d8f
Tahabz <etahabaz@gmail.com>
2022-04-07 00:53:54 +0000

    test: add getStudent test

--- test.js
+++ test.js
@@ -20,6 +20,36 @@ tape('health', async function (t) {
   }
 })
 
+tape('getStudent', async function (t) {
+  const url = `${endpoint}/student/44`
+  try {
+    const { data, response } = await jsonist.get(url)
+    if (response.statusCode !== 200) {
+      throw new Error('Error connecting to sqlite database; did you initialize it by running `npm run init-db`?')
+    }
+    t.deepEqual({
+      id: 44,
+      first_name: 'Elody',
+      last_name: 'Hudson',
+      email: 'Elody67@hotmail.com',
+      is_registered: 0,
+      is_approved: 1,
+      password_hash: '0190b9d5de6e72fa1a7b45f437a0384fe5c1a112',
+      address: '238 Hilll Garden Suite 822',
+      city: 'Cheyenne',
+      state: 'PA',
+      zip: '78421',
+      phone: '1-354-741-0049',
+      created: '1628789640104.0',
+      last_login: '1628746706627.0',
+      ip_address: '218.151.239.16'
+    }, data, 'should equal expected student')
+    t.end()
+  } catch (e) {
+    t.error(e)
+  }
+})
+
 tape('cleanup', function (t) {
   server.closeDB()
   server.close()
