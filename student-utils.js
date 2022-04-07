8eea04960cb81656d14af9056fa8b570ab2e3039
Tahabz <etahabaz@gmail.com>
2022-04-07 01:27:29 +0000

    fix: return nothing if student not found

--- student-utils.js
+++ student-utils.js
@@ -6,9 +6,10 @@ module.exports = {
 
 async function getStudentGradesAndDetails (grades, studentId) {
   const id = Number(studentId)
-  const student = (await knex('students').where({ id: studentId }))[0]
+  const student = (await knex('students').where({ id: studentId }))
+  if (student.length === 0) return
   const studentGrades = grades
     .filter((student) => student.id === id)
     .map(({ grade, course }) => ({ grade, course }))
-  return { ...student, grades: studentGrades }
+  return { ...student[0], grades: studentGrades }
 }
